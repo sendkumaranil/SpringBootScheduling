538d58023556b0c3f2d3b58151a939329601b55e
Anil Kumar <anilkumar@Anils-MacBook-Air.local>
2019-02-27 23:57:08 +0530

    added project

---
+++ src/main/java/com/springboot/scheduling/example/app/schedulejob/SpringBootScheduling.java
@@ -0,0 +1,22 @@
+package com.springboot.scheduling.example.app.schedulejob;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringBootScheduling {
+
+	private Logger logger=LoggerFactory.getLogger(SpringBootScheduling.class);
+	
+	@Scheduled(fixedRate=5000)
+	public void scheduleJob() {
+		logger.info("Printing message every 5 seconds");
+	}
+	
+	@Scheduled(cron="0 0 6,19 * * *")
+	public void scheduleCronJob() {
+		logger.info("Run this job every day at 6 am and 6 pm");
+	}
+}

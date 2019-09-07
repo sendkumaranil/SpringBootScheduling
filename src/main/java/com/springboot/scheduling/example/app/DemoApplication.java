25fade06c6631922865f0360e924dabf8eb633d1
Anil Kumar <sendkumaranil@gmail.com>
2019-09-07 13:11:14 +0530

    Update DemoApplication.java

--- src/main/java/com/springboot/scheduling/example/app/DemoApplication.java
+++ src/main/java/com/springboot/scheduling/example/app/DemoApplication.java
@@ -3,6 +3,9 @@ package com.springboot.scheduling.example.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+/**
+ Anil Kumar
+**/
 
 @SpringBootApplication
 @EnableScheduling
